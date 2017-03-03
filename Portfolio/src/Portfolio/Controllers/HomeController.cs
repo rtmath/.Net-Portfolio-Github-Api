f548da19b2f4e0ef15c047fd76ec053e4e31c692
Ryan Mathisen <ryantmathisen@gmail.com>
2017-03-03 10:22:37 -0800

    add Github API ajax call with successful response

--- Portfolio/src/Portfolio/Controllers/HomeController.cs
+++ Portfolio/src/Portfolio/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Portfolio.Controllers
 {
@@ -23,6 +28,37 @@ namespace Portfolio.Controllers
         {
 
             return View();
+        }
+
+        public JObject[] GetProjects()
+        {
+            string authKey = Authentication.basicAuthKey;
+
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/users/rtmath/starred");
+            request.AddParameter("client_id", Authentication.AuthId);
+            request.AddParameter("client_secret", Authentication.Secret);
+            request.AddHeader("User-Agent", "rtmath");
+            request.AddHeader("username", authKey);
+            request.AddHeader("Accept", "application/vnd.github.v3+json");
+            request.AddHeader("Accept", "application/vnd.github.inertia-preview+json");
+
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject[] jsonResponse = JsonConvert.DeserializeObject<JObject[]>(response.Content);
+            return jsonResponse;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
---
+++ Portfolio/src/Portfolio/Models/Authentication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class Authentication
+    {
+        public static string basicAuthKey = "a59faf78a4891478a6bf81de1365708785187dfa";
+        public static string AuthId = "52e0ae1a40854c2be50f";
+        public static string Secret = "150249b7a08624555fb986e65e787bf0b0889401";
+        //It's not really a secret now is it
+    }
+}
